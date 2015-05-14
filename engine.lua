633ba88d28f51ea00eedfaa7075f47f54c19c083
Breno Ramalho Lemes <breno@br-lemes.net>
2015-05-14 07:13:44 -0400

    Correção de bug: recurrent é um campo numérico no banco de dados, mas deve ser tratado como string na engine (tudo deve ser string).

--- engine.lua
+++ engine.lua
@@ -280,6 +280,7 @@ function eng.get_task(task)
 	local row = { }
 	cur:fetch(row, 'a')
 	cur:close()
+	row.recurrent = tostring(row.recurrent) -- it's a string number
 	return row
 end
 
@@ -415,6 +416,7 @@ function eng.go_next(task)
 			task.id))
 		return cur, err
 	end
+	return nil, "Should never happens"
 end
 
 -- return true if d is a valid date else return nil or false
