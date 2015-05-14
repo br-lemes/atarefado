5b899766e600b5f899bdf5fd53ad08b058c5b789
Breno Ramalho Lemes <breno@br-lemes.net>
2015-05-14 00:39:10 -0400

    Unfinished tasks handlers.

---
+++ webapp/newtask.lua
@@ -0,0 +1,77 @@
+<form class="form-horizontal" id="main" action=".?action=post&database=<%= html.dbactive %>" method="POST">
+	<fieldset>
+		<legend>Nova tarefa</legend>
+		<input type="hidden" name="action" value="new_task">
+		<div class="form-group">
+		  <label for="name" class="col-lg-2 control-label">Tarefa</label>
+		  <div class="col-lg-10">
+			<input class="form-control" id="name" name="name" type="text" autofocus>
+		  </div>
+		</div>
+		<div class="form-group">
+			<label for="date" class="col-lg-2 control-label">Data</label>
+			<div class="col-lg-10">
+				<input class="form-control" type="date" name="date">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="comment" class="col-lg-2 control-label">Comentários</label>
+			<div class="col-lg-10">
+				<textarea class="form-control" rows="7" name="comment"></textarea>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="tags" class="col-lg-2 control-label">Tags</label>
+			<div class="col-lg-10">
+				<select class="form-control" name="tags" size="7" multiple>
+				<% for i, v in ipairs(html.taglist) do  if i > 2 then %>
+					<option value="<%= v.id%>"><%= v.name %></option>
+				<% end end %>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="recurrent" class="col-lg-2 control-label">Recorrente</label>
+			<div class="col-lg-10">
+				<select class="form-control" name="recurrent">
+					<option value="1">Não</option>
+					<option value="2">Semanal</option>
+					<option value="3">Mensal</option>
+					<option value="4">Último dia</option>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="rweek" class="col-lg-2 control-label">Semanal</label>
+			<div class="col-lg-10">
+				<select class="form-control" name="rweek" size="7" multiple>
+					<option>Domingo</option>
+					<option>Segunda-feira</option>
+					<option>Terça-feira</option>
+					<option>Quarta-feira</option>
+					<option>Quinta-feira</option>
+					<option>Sexta-feira</option>
+					<option>Sábado</option>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="rmonth" class="col-lg-2 control-label">Mensal</label>
+			<div class="col-lg-10">
+				<select class="form-control" name="rmonth" size="7" multiple>
+				<% for i = 1, 31 do %>
+					<option><%= i %></option>
+				<% end %>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-lg-12">
+				<div class="pull-right">
+					<button class="btn" name="cancel">Cancelar</button>
+					<button class="btn btn-primary" type="submit">OK</button>
+				</div>
+			</div>
+		</div>
+	</fieldset>
+</form>
---
+++ webapp/updtask.lua
@@ -0,0 +1,77 @@
+<form class="form-horizontal" id="main" action=".?action=post&database=<%= html.dbactive %>" method="POST">
+	<fieldset>
+		<legend>Nova tarefa</legend>
+		<input type="hidden" name="action" value="upd_task">
+		<div class="form-group">
+		  <label for="name" class="col-lg-2 control-label">Tarefa</label>
+		  <div class="col-lg-10">
+			<input class="form-control" id="name" name="name" type="text" value="<%= task.name %>" autofocus>
+		  </div>
+		</div>
+		<div class="form-group">
+			<label for="date" class="col-lg-2 control-label">Data</label>
+			<div class="col-lg-10">
+				<input class="form-control" type="date" name="date" value="<%= task.date %>">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="comment" class="col-lg-2 control-label">Comentários</label>
+			<div class="col-lg-10">
+				<textarea class="form-control" rows="7" name="comment"><%= task.comment %></textarea>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="tags" class="col-lg-2 control-label">Tags</label>
+			<div class="col-lg-10">
+				<select class="form-control" name="tags[]" size="7" multiple>
+				<% for i, v in ipairs(html.taglist) do  if i > 2 then %>
+					<option value="<%= v.id%>" <% if tags[v.id] then %>selected<% end %>><%= v.name %></option>
+				<% end end %>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="recurrent" class="col-lg-2 control-label">Recorrente</label>
+			<div class="col-lg-10">
+				<select class="form-control" name="recurrent" value="<%= task.recurrent %>">
+					<option value="1">Não</option>
+					<option value="2">Semanal</option>
+					<option value="3">Mensal</option>
+					<option value="4">Último dia</option>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="rweek" class="col-lg-2 control-label">Semanal</label>
+			<div class="col-lg-10">
+				<select class="form-control" name="rweek[]" size="7" multiple>
+					<option value="1" <% if tags[1] then %>selected<% end %>>Domingo</option>
+					<option value="2" <% if tags[2] then %>selected<% end %>>Segunda-feira</option>
+					<option value="3" <% if tags[3] then %>selected<% end %>>Terça-feira</option>
+					<option value="4" <% if tags[4] then %>selected<% end %>>Quarta-feira</option>
+					<option value="5" <% if tags[5] then %>selected<% end %>>Quinta-feira</option>
+					<option value="6" <% if tags[6] then %>selected<% end %>>Sexta-feira</option>
+					<option value="7" <% if tags[7] then %>selected<% end %>>Sábado</option>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="rmonth" class="col-lg-2 control-label">Mensal</label>
+			<div class="col-lg-10">
+				<select class="form-control" name="rmonth[]" size="7" multiple>
+				<% for i = 1, 31 do %>
+					<option value="<%= i + 7 %>" <% if tags[i + 7] then %>selected<% end %>><%= i %></option>
+				<% end %>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-lg-12">
+				<div class="pull-right">
+					<button class="btn" name="cancel">Cancelar</button>
+					<button class="btn btn-primary" type="submit">OK</button>
+				</div>
+			</div>
+		</div>
+	</fieldset>
+</form>
