f5229dd8a106a2519976129b75a30a5efc1a9d46
Breno Ramalho Lemes <breno@br-lemes.net>
2020-06-04 01:13:08 -0400

    Acrescentado teste de id

--- test/engine.lua
+++ test/engine.lua
@@ -38,3 +38,19 @@ local function test_hastable()
 		assert(not eng.has_table(v), string.format("unexpected '%s' table found", v))
 	end
 end
+
+local function test_hasid()
+	local expected = "expected id number '%d' not found in table '%s'"
+	local unexpected = "unexpected id number '%d' found in table '%s'"
+	local list = { tagnames = 38, tags = 0, tasks = 0, options = 8 }
+	for k,v in pairs(list) do
+		if v == 0 then
+			assert(not eng.has_id(1, k), string.format(unexpected, 1, k))
+		else
+			for i = 1, v do
+				assert(eng.has_id(i, k), string.format(expected, i, k))
+			end
+			assert(not eng.has_id(v + 1, k), string.format(unexpected, v + 1, k))
+		end
+	end
+end
